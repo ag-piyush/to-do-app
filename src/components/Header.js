65aafc1b26e1ea072b4f31c047fbaebc871630b7
Piyush Agarwal <piyush.ag.bitsg@gmail.com>
2020-10-03 04:19:12 +0530

    Header update and App.css cleaning

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,21 @@
 import React from 'react'
 
 function Header() {
+    const date = new Date();
+    const hours = date.getHours();
+    let timeOfDay;
+    
+    if (hours<12) {
+        timeOfDay = "morning";
+    }
+    else if (hours < 17) {
+        timeOfDay = "afternoon";
+    }
+    else {
+        timeOfDay = "night";
+    }
     return (
-        <header class="navbar">This is the header</header>
+    <header class="navbar">Hey! Good {timeOfDay}</header>
     );
 }
 
