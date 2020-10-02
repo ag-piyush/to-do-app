f5a8e9a9f21229de5dd1997206d2ef6d88d18bed
Piyush Agarwal <piyush.ag.bitsg@gmail.com>
2020-10-03 04:38:48 +0530

    Minor updates in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,18 +4,30 @@ function Header() {
     const date = new Date();
     const hours = date.getHours();
     let timeOfDay;
+    let colorOfDay;
     
     if (hours<12) {
         timeOfDay = "morning";
+        colorOfDay = "white";
     }
     else if (hours < 17) {
         timeOfDay = "afternoon";
+        colorOfDay = "orange";
     }
-    else {
+    else if (hours < 22){
+        timeOfDay = "evening";
+        colorOfDay = "purple";
+    }
+    else{
         timeOfDay = "night";
+        colorOfDay = "black";
+    }
+
+    const styles = {
+        color: colorOfDay
     }
     return (
-    <header class="navbar">Hey! Good {timeOfDay}</header>
+    <header class="navbar" style={styles}>Hey! Good {timeOfDay}</header>
     );
 }
 
