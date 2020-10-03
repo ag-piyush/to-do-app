47e6ef0f8804b96aceb946d783f9a327cdfd05a6
Piyush Agarwal <piyush.ag.bitsg@gmail.com>
2020-10-03 14:29:06 +0530

    Added placeholder data

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,7 +4,7 @@ function TodoItem() {
     return(
         <div className="todo-item">
             <input type="checkbox" />
-            <p>Hello</p>
+            <p>PlaceHolder data</p>
         </div>
     );
 }
