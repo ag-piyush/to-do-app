d8d5782d3264900f751c27ccd93c321eaf6ff572
Piyush Agarwal <piyush.ag.bitsg@gmail.com>
2020-10-03 06:49:07 +0530

    Updates

--- precache-manifest.162191b06974c7e1b17db1131cf8cfd2.js
+++ precache-manifest.162191b06974c7e1b17db1131cf8cfd2.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "89144a29944242c05ff60b9874209b2d",
+    "revision": "31e73b074a0eaa740960b5bab9065bea",
     "url": "/to-do-app/index.html"
   },
   {
-    "revision": "b96109489271888db21f",
+    "revision": "6c8e56875575cb41a28f",
     "url": "/to-do-app/static/css/main.cf9044a7.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/to-do-app/static/js/2.e58d75b1.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b96109489271888db21f",
-    "url": "/to-do-app/static/js/main.c36d456c.chunk.js"
+    "revision": "6c8e56875575cb41a28f",
+    "url": "/to-do-app/static/js/main.c99d2428.chunk.js"
   },
   {
     "revision": "4385e1bd226fdb470057",
